3952afc98001bdc855bfe9350045b0ac19ecea52
NikolasTh90 <41706535+NikolasTh90@users.noreply.github.com>
2023-10-28 21:45:57 +0300

    pain

---
+++ HW3/NeuralNetwork.py
@@ -0,0 +1,170 @@
+from Neuron import Neuron
+import numpy as np
+from matplotlib import pyplot as plt
+import pickle
+import sys
+from multiprocessing import Pool, cpu_count
+import time
+class SOMNeuralNetwork:
+    def __init__(self, parameters: dict):
+        self.parameters = parameters
+        self.gridSize = int(parameters['gridSize'])
+        self.numEpochs = int(parameters['numEpochs'])
+        self.learningRate = parameters["learningRate"]
+        self.numInputNeurons = int(parameters['numInputNeurons'])
+        self.gaussianRadius = parameters["gridSize"]/2.0
+        self.currentPattern = 0
+        self.currentEpoch = 0
+        self.inputNeurons = self.createInputNeurons()
+        self.TrainingError = []
+        self.TestingError = []
+        self.initializeWeights()
+
+    def appendTrainingError(self, trainingError):
+        self.TrainingError.append(trainingError)
+    
+    def appendTestingError(self, testingError):
+        self.TestingError.append(testingError)
+
+
+    def createInputNeurons(self):
+        inputNeurons = []
+        for i in range(self.numInputNeurons):
+            inputNeurons.append(Neuron())
+        return inputNeurons
+
+    def initializeWeights(self):
+        for inputNeuron in self.inputNeurons:
+            # Initialize weights for each input neuron
+            inputNeuron.setWeights(np.random.uniform(0, 1, (self.gridSize, self.gridSize)))
+
+    def setInputs(self, inputs):
+        for i, inputNeuron in enumerate(self.inputNeurons):
+            inputNeuron.setOutput(inputs[f'in{i+1}'])
+
+    def findBMU(self):
+        input_outputs = np.array([neuron.getOutput() for neuron in self.inputNeurons])
+        input_weights = np.array([neuron.getWeights() for neuron in self.inputNeurons])
+        distances = np.sum((input_outputs[:, np.newaxis, np.newaxis] - input_weights) ** 2, axis=0)
+        return np.unravel_index(np.argmin(distances), distances.shape)
+
+    # def findBMU(self):
+    #     distances = np.zeros((self.gridSize, self.gridSize))
+    #     for inputNeuron in self.inputNeurons:
+    #         for i in range(self.gridSize):
+    #             for j in range(self.gridSize):
+    #                 distances[i][j] += (inputNeuron.getOutput() - inputNeuron.getWeights()[i][j]) ** 2
+
+    #     return np.unravel_index(np.argmin(distances), distances.shape)
+    
+    def updateLearningRate(self, epoch):
+        self.learningRate = self.learningRate * np.exp(-epoch / self.numEpochs)
+    
+    def updateGaussianRadius(self, epoch):
+        self.gaussianRadius = self.parameters["gridSize"]/2.0 * np.exp(-epoch / self.numEpochs)
+    
+    # def updateWeights(self, bmu, epoch):
+    #     for inputNeuron in self.inputNeurons:
+    #         for i in range(self.gridSize):
+    #             for j in range(self.gridSize):
+    #                 distance = np.sqrt((bmu[0]-i)**2 + (bmu[1]-j)**2)
+    #                 if distance <= self.gaussianRadius:
+    #                     influence = np.exp(-(distance**2)/(2*(self.gaussianRadius**2)))
+    #                     inputNeuron.setWeight(i, j, inputNeuron.getWeights()[i][j] + self.learningRate * influence * (inputNeuron.getOutput() - inputNeuron.getWeights()[i][j]))
+
+
+    def updateWeights(self, bmu, epoch):
+        for inputNeuron in self.inputNeurons:
+            i, j = np.meshgrid(range(self.gridSize), range(self.gridSize), indexing='ij')
+            distance = np.sqrt((bmu[0] - i) ** 2 + (bmu[1] - j) ** 2)
+            mask = distance <= self.gaussianRadius
+            influence = np.exp(-distance ** 2 / (2 * self.gaussianRadius ** 2))
+            delta_weights = self.learningRate * influence * (inputNeuron.getOutput() - inputNeuron.getWeights())
+            inputNeuron.setWeights(inputNeuron.getWeights() + mask * delta_weights)
+
+
+
+    # visualize the som map
+    def visualize_map(self, input_data):
+        before = time.time()
+        # Create a grid to represent the SOM
+        som_grid = np.zeros((self.gridSize, self.gridSize), dtype=object)
+
+        # Find the winning neuron for each input and store it in the grid
+
+        for i in range(self.gridSize):
+            for j in range(self.gridSize):
+                print(f"Node ({i},{j})")
+
+                minDistanceLetter = ''
+                minDistance = sys.maxsize
+                distance = 0
+
+                for input in range(len(input_data[:5000])):
+                        # print(f"Input ({input})", end="\r")
+                        distance = np.sum((input_data[input][f'in{k}'] - self.inputNeurons[k - 1].getWeights()[i][j]) ** 2 for k in range(1, 17))
+                        if distance < minDistance:
+                            minDistance = distance
+                            minDistanceLetter = input_data[input]['out']
+
+                som_grid[i][j] = minDistanceLetter
+        after = time.time()
+        print(f"Time taken: {after - before}")
+        # Create a plot to visualize the SOM
+        plt.figure(figsize=(self.gridSize, self.gridSize))
+        for x in range(self.gridSize):
+            for y in range(self.gridSize):
+                try:
+                    plt.text(x+0.2 if x<self.gridSize-1 else x-0.2, y+0.2 if y<self.gridSize-1 else y-0.2, som_grid[x, y] if som_grid[x, y] else ' ', va='center', ha='center')
+                except:
+                    plt.text(x+0.2 if x<self.gridSize-1 else x-0.2, y+0.2 if y<self.gridSize-1 else y-0.2, ' ', va='center', ha='center')
+
+        plt.xticks(range(self.gridSize))
+        plt.yticks(range(self.gridSize))
+        plt.grid()
+        plt.title("SOM Classification of Alphabet Letters")
+        plt.show()
+
+    
+    def plotTrainingError(self):
+        plt.plot(self.TrainingError)
+        plt.title("Training Error")
+        plt.xlabel("Epoch")
+        plt.ylabel("Error")
+        plt.show()
+    
+    def plotTestingError(self):
+        plt.plot(self.TestingError)
+        plt.title("Testing Error")
+        plt.xlabel("Epoch")
+        plt.ylabel("Error")
+        plt.show()
+
+
+    def getGrid(self):
+        return self.grid
+    
+    def getInputNeurons(self):
+        return self.inputNeurons
+
+    def getLearningRate(self):
+        return self.learningRate
+    
+    def save(self, filename):
+        # Serialize the entire object using pickle
+        with open(filename, 'wb') as file:
+            pickle.dump(self, file)
+
+
+    def __str__(self):
+        nodes = ""
+        for input in self.inputNeurons:
+            nodes += "Neuron: " + str(input.id) + "\n"
+            nodes += "Weights: " + str(input.getWeights()) + "\n"
+            nodes += "Connected To: " + str(input.getConnectedToNeurons()) + "\n"
+        nodes += "\n------------------------\n"
+            
+        return "Neural Network: \n" + \
+            "Parameters: " + str(self.parameters) + "\n" + \
+            "Nodes: " + "\n" + nodes
+            
---
+++ HW3/Trainer.py
@@ -0,0 +1,80 @@
+from math import sqrt
+from FileManager import FileReader, FileWriter
+from NeuralNetwork import SOMNeuralNetwork
+import numpy as np
+from utility import TARGET_OUTPUTS, prepare_data
+import time
+import multiprocessing
+import pickle
+max_workers=multiprocessing.cpu_count()
+print(max_workers)
+def getParameters(filename='parameters.txt'):
+    parameters = FileReader('parameters.txt').getParameters()
+    return parameters
+
+def load_som_model(filename):
+    with open(filename, 'rb') as file:
+        som = pickle.load(file)
+    return som
+
+def calculate_error(data, som):
+    error = 0
+    for i in range(len(data)):
+        som.setInputs(data[i])
+        bmu = som.findBMU()
+        error += sqrt(sum((data[i][f'in{k}'] - som.inputNeurons[k - 1].getWeights()[bmu[0]][bmu[1]]) ** 2 for k in range(1, 17)))
+    error /= len(data)
+
+    return error
+
+def train(filename='all_data.txt', epochs=None):
+    parameters = getParameters()
+    data = prepare_data(filename)
+    print('Training Data Length:', len(data['trainingData']))
+
+    SOM = load_som_model('som_np_paxas.pkl')
+    # SOM = SOMNeuralNetwork(parameters)
+
+    SOM.currentEpoch = SOM.currentEpoch
+    try:
+       while SOM.currentEpoch < int(parameters["numEpochs"]):
+            print(f'Epoch {SOM.currentEpoch + 1}')
+            start_time = time.time()
+            while SOM.currentPattern < len(data['trainingData']):
+                print(f"Progress: {SOM.currentPattern}/{len(data['trainingData'])}", end="\r")
+                SOM.setInputs(data["trainingData"][SOM.currentPattern])
+                bmu = SOM.findBMU()
+                SOM.updateLearningRate(SOM.currentEpoch)
+                SOM.updateGaussianRadius(SOM.currentEpoch)
+                SOM.updateWeights(bmu, SOM.currentEpoch)
+                SOM.currentPattern += 1
+            end_time = time.time()
+            print(f'Epoch {SOM.currentEpoch + 1} completed in {end_time - start_time} seconds')
+            
+            start_time = time.time()
+            train_error = calculate_error(data['trainingData'], SOM)
+            SOM.appendTrainingError(train_error)
+            end_time = time.time()
+            print(f'Epoch {SOM.currentEpoch+1} training error calculated in {end_time - start_time} seconds')
+            print(f"Train Error={train_error}")
+            start_time = time.time()
+            test_error = calculate_error(data['testData'], SOM)
+            SOM.appendTestingError(test_error)
+            end_time = time.time()
+            print(f'Epoch {SOM.currentEpoch+1} testing error calculated in {end_time - start_time} seconds')
+            print(f"Test Error={test_error}")
+            SOM.currentPattern = 0
+            SOM.currentEpoch += 1
+            
+    except KeyboardInterrupt:
+        print("Training interrupted")
+    
+    print("Training complete")
+    SOM.save("som_np_paxas.pkl")
+    SOM.plotTrainingError()
+    SOM.plotTestingError()
+    SOM.visualize_map(data['trainingData'])
+    
+np.random.seed(4)    
+train()
+
